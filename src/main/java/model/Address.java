f6a7fca49b0365e75662b83ff3d283dfbd754b48
modafro <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-03-20 22:27:26 +0200

    Create Models classes

---
+++ src/main/java/model/Address.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Address {
+    private String country;
+    private String city;
+    private String region;
+    private String street;
+    private String building;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+}
---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String email;
+    private int salary;
+    private List<Address> addresses;
+    private List<Phone> phones;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+}
